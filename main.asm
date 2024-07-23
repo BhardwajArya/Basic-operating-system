89ec0b7ee17df0e29e8bf7259fa0e064013a8135
BhardwajArya <bhardwajarya01@gmail.com>
2024-07-23 19:19:54 +0530

    Add files via upload

---
+++ main.asm
@@ -0,0 +1,18 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2761
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww18160\viewh10800\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 org 0x7C00\
+bits 16x\
+\
+main:\
+       hlt\
+\
+.halt:\
+       jmp.halt\
+\
+times 510-($-$$) db 0\
+dw 0AA55h}
